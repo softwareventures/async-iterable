7f8c84537678f7dd372b8909c670d6fd49b89805
Daniel Cassidy <mail@danielcassidy.me.uk>
2022-10-07 01:57:30 +0100

    feat(asyncIterable): add asyncIterable constructor function

--- index.ts
+++ index.ts
@@ -3,3 +3,9 @@ export type AsyncIterableLike<T> =
     | Iterable<T | Promise<T>>
     | Promise<AsyncIterable<T>>
     | Promise<Iterable<T | Promise<T>>>;
+
+export async function* asyncIterable<T>(iterable: AsyncIterableLike<T>): AsyncIterable<T> {
+    for await (const element of await iterable) {
+        yield element;
+    }
+}
